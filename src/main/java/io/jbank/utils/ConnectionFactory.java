5357e1c2e9ed0c6a844b76c633f206ed5f3427b6
Gabriel Marques <59850744+gabriel0x01@users.noreply.github.com>
2024-01-12 23:38:51 -0300

    Refactor database connection using HikariCP

--- src/main/java/io/jbank/utils/ConnectionFactory.java
+++ src/main/java/io/jbank/utils/ConnectionFactory.java
@@ -1,20 +1,33 @@
 package io.jbank.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionFactory {
 
     public Connection abrirConexao() {
         try {
-            Connection connection = DriverManager.getConnection();
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            return connection;
+            return createDataSource().getConnection();
 
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() throws ClassNotFoundException {
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://aws.connect.psdb.cloud/jbank?sslMode=VERIFY_IDENTITY");
+        config.setUsername("6np3l6z4ef98xva123s6");
+        config.setPassword("pscale_pw_LrZIf95JyYwEdJV8cUauAz6Id9S0iQNAK5CjmDCF0Xf");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 
     public void fecharConexao(Connection connection) {
